ade42bb0b0c74a33f898ab964e4273a92ced9546
alvaromgueiros <alvaromgueiros@gmail.com>
2019-09-03 14:01:15 -0300

    criei um component com meu nome

--- projeto/src/app/app.module.ts
+++ projeto/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TaliadinizComponent } from './taliadiniz/taliadiniz.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaliadinizComponent
   ],
   imports: [
     BrowserModule
---
+++ projeto/src/app/taliadiniz/taliadiniz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-taliadiniz',
+  templateUrl: './taliadiniz.component.html',
+  styleUrls: ['./taliadiniz.component.css']
+})
+export class TaliadinizComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
